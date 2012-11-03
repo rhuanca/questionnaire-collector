cf40985cf756c1ceae1d8e66bf32270a9bcbbacf
Renan Huanca <rhuanca@gmail.com>
2012-11-03 13:40:11 -0400

    Minor improvements.

--- routes/collect.js
+++ routes/collect.js
@@ -6,3 +6,10 @@
 exports.list = function(req, res){
 	res.render('collect', { data: 'data' });
 };
+
+exports.addQuestion = function(req, res){
+	//res.render('collect', { data: 'data' });
+	res.send('hello again');
+};
+
+
