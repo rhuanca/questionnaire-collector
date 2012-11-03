789a4df2a7c1fd0d8c217d508048848c5ac788e7
Renan Huanca <rhuanca@gmail.com>
2012-11-03 19:26:19 -0400

    now initial ajax call works.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/collect', collect.list);
-app.get('/collect/question', collect.addQuestion);
+app.post('/collect/question', collect.addQuestion);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
