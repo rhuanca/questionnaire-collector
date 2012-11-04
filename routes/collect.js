b87194332033ce115833fe8eed960ad91b0ce253
Renan Huanca <rhuanca@gmail.com>
2012-11-03 21:50:27 -0400

    starting to work with mongodb.

--- routes/collect.js
+++ routes/collect.js
@@ -3,13 +3,28 @@
  * collect.
  */
 
+
+
+var mongoose = require('mongoose')
+, db = mongoose.createConnection('localhost', 'questionnaire');
+
+var Question = new mongoose.Schema({
+    question: String
+});
 exports.list = function(req, res){
 	res.render('collect', { data: 'data' });
 };
 
 exports.addQuestion = function(req, res){
-	//res.render('collect', { data: 'data' });
-	res.send('hello again');
+
+	var question = new Question({ question: req.body.question });
+	
+	question.save(function (err){
+		if(err)
+			console.log("Some error happen");	
+	});
+
+	res.send('Question added :'+question.question + ".");
 };
 
 
