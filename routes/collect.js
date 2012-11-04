02a7b4191ef50bba72fd6608522c9bf125dc63e3
Renan Huanca <rhuanca@gmail.com>
2012-11-03 23:21:07 -0400

    now it is possible to add a question.

--- routes/collect.js
+++ routes/collect.js
@@ -3,27 +3,30 @@
  * collect.
  */
 
+var mongo = require('mongodb'),
+  Server = mongo.Server,
+  Db = mongo.Db;
 
+var db = new Db('questionnaire', new Server('localhost', 27017, {auto_reconnect: true}));
 
-var mongoose = require('mongoose')
-, db = mongoose.createConnection('localhost', 'questionnaire');
-
-var Question = new mongoose.Schema({
-    question: String
+db.open(function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
 });
+
 exports.list = function(req, res){
 	res.render('collect', { data: 'data' });
 };
 
 exports.addQuestion = function(req, res){
 
-	var question = new Question({ question: req.body.question });
-	
-	question.save(function (err){
-		if(err)
-			console.log("Some error happen");	
-	});
+	var question = { 'question': req.body.question };
 
+	db.collection('questions', function(err, collection) {
+		collection.insert(question);
+	});	
+	
 	res.send('Question added :'+question.question + ".");
 };
 
