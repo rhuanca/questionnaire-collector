7a0f5e27b3829cd973b9c0149eccc66eece87052
Renan Huanca <rhuanca@gmail.com>
2012-11-16 02:04:01 -0400

    now it is possible to store authors.

--- routes/questions.js
+++ routes/questions.js
@@ -3,6 +3,14 @@
  * collect.
  */
 
+function storeAuthor(authorName){
+	db.collection('authors').findOne({'name': authorName}, function(err, doc) {
+		if(doc == null) {
+			console.log("Creating new author" + authorName);
+			db.collection('authors').insert({name:authorName});
+		}
+	});
+}
 
 exports.edit = function(req, res){
 	res.render('question-edit', { data: 'data' });
@@ -11,13 +19,17 @@ exports.edit = function(req, res){
 exports.add = function(req, res){
 	var question = req.body;
 	var counters = db.collection("counters");
+	
 	counters.findAndModify( {'_id':'questions'},[], {'$inc': { 'next': 1 }}, function(err,doc) {
 		var questionId = doc.next;
 		console.log("next question id = " + questionId);
 		question._id = questionId;
-		db.collection('questions', function(err, collection) {
-			collection.insert(question);
-		});
+		
+		var questions = db.collection('questions');
+		questions.insert(question);
+		
+		storeAuthor(question.questionAuthor);
+		
 		res.send(question);
 	});
 };
@@ -27,6 +39,9 @@ exports.update = function(req, res){
 	var questions = db.collection("questions");
 	console.log("to update = " + req.params.id);
 	questions.update({'_id': parseInt(req.params.id, 10)}, question);
+	
+	storeAuthor(question.questionAuthor);
+	
 	questions.findOne({'_id': parseInt(req.params.id, 10)}, {}, function(err, doc){
 		res.send(question);
 	});
@@ -41,3 +56,5 @@ db.authors.insert({name:'renan',cookie:''});
 
 supervisor app.js 
 */
+
+//var author = authors.findOne({'name': authorName});
